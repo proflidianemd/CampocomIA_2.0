ad623d3d769a41bf8acbda1b03f45eb1664ef6f6
Lidiane Yumi <127134394+proflidianemd@users.noreply.github.com>
2026-05-17 16:05:00 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,56 @@
+// ACCORDION
+const headers = document.querySelectorAll(".accordion-header");
+
+headers.forEach(header => {
+
+  header.addEventListener("click", () => {
+
+    const content = header.nextElementSibling;
+
+    if(content.style.display === "block"){
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+
+  });
+
+});
+
+// ACESSIBILIDADE
+let tamanhoFonte = 16;
+
+function aumentarFonte(){
+
+  tamanhoFonte += 1;
+
+  document.body.style.fontSize = tamanhoFonte + "px";
+
+}
+
+function diminuirFonte(){
+
+  tamanhoFonte -= 1;
+
+  document.body.style.fontSize = tamanhoFonte + "px";
+
+}
+
+function alternarTema(){
+
+  document.body.classList.toggle("dark");
+
+}
+
+// LEITURA DE VOZ
+function lerTexto(){
+
+  const texto = document.body.innerText;
+
+  const fala = new SpeechSynthesisUtterance(texto);
+
+  fala.lang = "pt-BR";
+
+  speechSynthesis.speak(fala);
+
+}
